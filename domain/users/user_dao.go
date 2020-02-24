d2e3c5247307d200d7c4732466e4f787ec5973ab
dmolina79 <doug.molinam@gmail.com>
2020-02-23 21:24:11 -0600

    adds get user function

--- domain/users/user_dao.go
+++ domain/users/user_dao.go
@@ -1,42 +1,53 @@
 package users
 
 import (
+	"database/sql"
 	"fmt"
 	"github.com/dmolina79/bookstore_users-api/datasources/mysql/users_db"
 	"github.com/dmolina79/bookstore_users-api/utils/date"
 	"github.com/dmolina79/bookstore_users-api/utils/errors"
-	"log"
+	"strings"
 )
 
 const (
+	uniqueEmailError = "email_UNIQUE"
+	errorNoRows = "no rows in result set"
 	queryInsertUser = "INSERT INTO users(first_name, last_name, email, date_created) VALUES(?,?,?,?);"
+	queryGetUser = "SELECT id, first_name, last_name, email, date_created FROM users WHERE id=?;"
 )
 
 var (
 	usersDB = make(map[int64]*User)
 )
 
+func mapUserFromRow(row *sql.Row, user *User) error {
+	return row.Scan(&user.Id, &user.FirstName, &user.LastName, &user.Email, &user.DateCreated)
+}
+
 func (user *User) Get() *errors.RestErr {
-	if err := users_db.Client.Ping(); err != nil {
-		panic(err)
+	stmt, err := users_db.Client.Prepare(queryGetUser)
+	if err != nil {
+		return errors.NewInternalServer(err.Error())
 	}
-	result := usersDB[user.Id]
-	if result == nil {
-		return errors.NewNotFound(fmt.Sprintf("user %d not found", user.Id))
+	defer stmt.Close()
+
+	row := stmt.QueryRow(user.Id)
+	if err := mapUserFromRow(row, user); err != nil {
+		if strings.Contains(err.Error(), errorNoRows) {
+			return errors.NewNotFound(fmt.Sprintf("user %d not found", user.Id))
+		}
+		fmt.Println(err)
+		return errors.NewInternalServer(fmt.Sprintf("error when trying to get user %d %s", user.Id, err.Error()))
 	}
-	user.Id = result.Id
-	user.FirstName = result.FirstName
-	user.LastName = result.LastName
-	user.Email = result.Email
-	user.DateCreated = result.DateCreated
 
 	return nil
 }
 
+
+
 func (user *User) Save() *errors.RestErr {
 	stmt, err := users_db.Client.Prepare(queryInsertUser)
 	if err != nil {
-		log.Println("entre a error de prepare")
 		return errors.NewInternalServer(err.Error())
 	}
 	defer stmt.Close()
@@ -51,6 +62,9 @@ func (user *User) Save() *errors.RestErr {
 	)
 
 	if err != nil {
+		if strings.Contains(err.Error(), uniqueEmailError) {
+			return errors.NewBadRequest(fmt.Sprintf("email %s is already registered", user.Email))
+		}
 		return errors.NewInternalServer(fmt.Sprintf("error when trying to save user: %s", err.Error()))
 	}
 
